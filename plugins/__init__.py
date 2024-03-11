0b6b594c09ad41241431da9ae4bdcda48fbc219f
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-03-11 12:40:19 +0530

    𝐁𝐑𝐀𝐍𝐃𝐄𝐃 𝐌𝐀𝐒𝐒 𝐑𝐄𝐏𝐎𝐑𝐓

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("iam_daxx")
+    return web.json_response("BRANDEDKING82")
 
 
 async def web_server():
